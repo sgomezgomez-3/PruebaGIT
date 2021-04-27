c8aa6efd55dd9e5a788ca9c817c4c292315271bc
santos <santitos3wp@gmail.com>
2021-04-27 18:28:08 +0200

    Comentario

---
+++ src/Ejemplo.java
@@ -0,0 +1,11 @@
+
+public class Ejemplo {
+	/**
+	 * Esto es un javadoc
+	 * @param args es un conjunto de parametros 1
+	 */
+	public Ejemplo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
